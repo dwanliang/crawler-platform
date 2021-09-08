e7e26aa802f37f92a5af1505ebd03f7f7c3b05a1
liangziyan <lzy@minstone.com.cn>
2021-09-08 18:26:00 +0800

    新增选项字典

---
+++ src/assets/js/dictionaries.js
@@ -0,0 +1,31 @@
+export const typeList = [
+  {
+    value: '1',
+    label: '文本'
+  }, {
+    value: '2',
+    label: '多行文本'
+  }, {
+    value: '3',
+    label: '单选'
+  }, {
+    value: '4',
+    label: '多选'
+  }, {
+    value: '5',
+    label: '时间'
+  }, {
+    value: '6',
+    label: '日期'
+  }
+]
+
+export const rulesList = [
+  {
+    value: '1',
+    label: '文本'
+  }, {
+    value: '2',
+    label: '数字'
+  }
+]
