cc6bb2be94c84fb15fc30ebdd9d3b90a05b7f702
liangziyan <lzy@minstone.com.cn>
2021-09-08 18:26:52 +0800

    新增提示字典

---
+++ src/assets/js/explain.js
@@ -0,0 +1,8 @@
+const explainList = {
+  formTitle:'标题，用来提示填写者需要输入什么内容',
+  formReplace:'文档中的占位符单词',
+  formValue:'表单的默认值',
+  formPlaceholder:'输入框内的提示文字',
+  formtips:'提示内容',
+}
+export default explainList;
